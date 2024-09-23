2b273e260cffb589f8e93f2ee2414c02ccf73944
Spingrid <118059301+Spingrid@users.noreply.github.com>
2024-09-23 13:39:43 +0100

    Update page.js

--- page.js
+++ page.js
@@ -74,3 +74,30 @@ const ensurePageChange = setInterval(() => {
 document.body.replaceWith(document.body.cloneNode(true));
 
 });
+
+function removeElement() {
+    
+    const element = document.querySelector('.convertflow-cta.cf-overlay');
+    
+    
+    if (element) {
+        setTimeout(() => {
+            element.remove();
+            console.log('Element with class "convertflow-cta cf-overlay" has been removed.');
+        }, 200); 
+    }
+}
+
+
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach(() => {
+       
+        removeElement();
+    });
+});
+
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+
+removeElement();
